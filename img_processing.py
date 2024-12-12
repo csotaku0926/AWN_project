08e595f25c8aea9603916b0afaba7a44892628ca
cs_otaku <ben900926@gmail.com>
2024-12-12 22:32:21 +0800

    new file

---
+++ img_processing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import h5py
+import os
+
+SCENARIO = "colo_cams"
+IMG_DIR = f"../../data/single_scenario/{SCENARIO}"
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
